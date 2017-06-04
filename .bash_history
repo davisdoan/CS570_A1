7876c7cd29336193dc8e52597546961286ddc947
davisdoan <davisdoan@gmail.com>
2017-06-04 12:02:21 -0700

    added in working threads

--- .bash_history
+++ .bash_history
@@ -7,3 +7,94 @@ ls
 mkdir practice1
 ls
 exit
+ls
+touch test.c
+vim test.c 
+g++ test.c 
+ls
+./a.out 
+history
+vim test.c 
+ls
+vim test.c 
+g++ test.c 
+vim test.c 
+g++ test.c 
+./a.out 
+ls
+vim QUOTE.txt 
+vim test.c 
+g++ test.c 
+vim test.c 
+g++ test.c 
+vim test.c 
+g++ test.c 
+vim test.c 
+g++ test.c 
+./a.out 
+ls
+vim QUOTE.txt 
+vim test.c 
+g++ test.c 
+vim test.c 
+g++ test.c 
+vim test.c 
+g++ test.c 
+vim test.c 
+g++ test.c 
+gcc -pthread test.c 
+g++ -pthread test.c 
+ls
+vim test.c 
+g++ -pthread test.c 
+./a.out 
+vim test.c 
+g++ -pthread test.c 
+vim test.c 
+g++ -pthread test.c 
+vim test.c 
+g++ -pthread test.c 
+vim test.c 
+g++ -pthread test.c 
+ls
+./a.out 
+vim test.c
+g++ -pthread test.c 
+./a.out 
+vim test.c
+g++ -pthread test.c 
+./a.out 
+clear
+ls
+vim test.c
+g++ -pthread test.c 
+vim test.c
+g++ -pthread test.c 
+./a.out 
+vim test.c 
+g++ -pthread test.c 
+./a.out 
+vim test.c 
+ls
+mv test.c main.c
+g++ -pthread main.c 
+./a.out 
+git init
+git commit -m "initial commit for A1"
+git remote -add origin https://github.com/davisdoan/CS570_A1.git
+
+git commit -m "initial commit for A1"
+git remote -add origin https://github.com/davisdoan/CS570_A1.git
+git config -- global user.name "davisdoan"
+git config --global user.name "davisdoan"
+git config --global user.email "davisdoan@gmail.com"
+git status
+git remote -add origin https://github.com/davisdoan/CS570_A1.git
+git remote add origin https://github.com/davisdoan/CS570_A1.git
+git push origin master
+clear
+ls
+im main.c 
+vim main.c 
+g++ -pthread main.c 
+vim main.c 
--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <semaphore.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <string>
+
 using namespace std;
 
 #define N 7
@@ -11,6 +13,38 @@ using namespace std;
 void *worker_thread(void *arg)
 {
     printf("This is worker_thread()\n");
+    //open the quote file and write threads id followed by "THE QUOTE"(followed by carriage return and NEWLINE)
+     //FILE *fp=fopen("QUOTE.TXT", "a");
+//     file.open("QUOTE.txt");
+     // get the thread id
+     
+     ofstream file;
+     long threadId;
+     
+     // quotes
+     char evenQuote[100]="Controlling complexity is the essence of computer programming. --Brian Kernigan";     
+     char oddQuote[150]="Computer science is no more about computers than astronomy is about telescopes. --Edsger Dikstra";
+     
+     threadId = (long)arg;
+
+
+     
+     file.open("QUOTE.txt", std::ios_base::app);
+     
+     // if even
+     if(threadId % 2 == 0){
+       file << "Thread id " << threadId << " " << evenQuote << "\r\n";
+     } else {
+       file << "Thread id " << threadId << " " << oddQuote <<  "\r\n";
+     }
+     // if odd
+
+     file.close();
+
+     //write to the console "Thread <id> is running" followed by a new line
+    //close the quote.txt file
+    //release the semaphore flag
+    //repeat the above 7 times
     pthread_exit(NULL);
 }
 
@@ -20,7 +54,7 @@ int main(){
     int myPid;
     myPid = getpid(); // grab pid
     file.open("QUOTE.txt");
-    file <<  myPid; // write pid to the file
+    file << "Your Process PID is " <<  myPid << "\r\n"; // write pid to the file
     file.close();
 
     // create semaphore
@@ -32,16 +66,36 @@ int main(){
     
     printf("in main: creating thread \n");
     
-    long id;
+   // initialize thread attribute
+   pthread_attr_t myAttr;
+   pthread_attr_init(&myAttr);
+   pthread_attr_setdetachstate(&myAttr, PTHREAD_CREATE_JOINABLE);
+
+   long id;
     for(id=1; id <=N; id++){
-      int ret = pthread_create(&my_thread[id],NULL, &worker_thread, (void*)id);
+      //make the seven threads 
+      int ret = pthread_create(&my_thread[id], &myAttr, &worker_thread, (void*)id);
+//      pthread_join(&my_thread[id],NULL);
       if(ret != 0) {
           printf("Error: pthread_Create() failed!\n");
           exit(EXIT_FAILURE);
        }   
      }
 
-   pthread_exit(NULL);
+   // free resources used by the attribute
+   pthread_attr_destroy(&myAttr); 
 
+   // join all the threads
+   void *status; 
+   for(id=1; id <=N; id++){
+       pthread_join(my_thread[id],&status);
+   }
+   
+   // semaphore destroy
+   sem_destroy(&mySemaphore); 
+   
+   // exit gracefully
+   printf("Job Completed, bye!\n");
+   pthread_exit(NULL);
     return 0;
 }
